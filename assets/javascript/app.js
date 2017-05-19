9efb53718180fdcec585786137e7d9f75781705c
novialim <novialim@gmail.com>
2017-05-19 15:55:32 -0700

    Adding github login

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,8 +39,40 @@
 		});
 
 		//Set interval every min
+		interval = setInterval(refreshTable, 60000);
 
 	}); // End of initial load
+
+	$("#signInWithGithub").on("click", function(event){
+
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+		  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+		  var token = result.credential.accessToken;
+		  // The signed-in user info.
+		  var user = result.user;
+		  
+		  console.log("Signed in!");
+
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+	  		console.log("Error - " + errorCode + "  " + errorMessage + "  " + email + "  " + credential);
+		});
+
+	}); 
+
+	function refreshTable(){
+		$("#scheduleDetails").empty();
+		allTrains.forEach(function(snapshotdata){
+			updateTrainTable(snapshotdata[1],snapshotdata[0]);
+		});
+	}
+
 
 	// Add new train
 	$("#addTrain").on("click", function(event){
